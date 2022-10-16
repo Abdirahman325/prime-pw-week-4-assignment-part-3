ec3de6ef8fd13553c75d3ffaad85d4236efe723f
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-16 15:35:18 -0500

    heating up

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,15 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket =[];
+const maxItems = 5;
+//stretch goals
+
+function isfull();
+ if ( basket.length < maxItems){
+    return false;
+ } 
+    else if (basket.length >= maxItems){
+        return true;
+ }
