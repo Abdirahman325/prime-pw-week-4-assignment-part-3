30eec015c2edb365ac98238fd3fa900f18c03970
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-16 19:41:52 -0500

    im getting there

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,49 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-let basket =[];
-const maxItems = 5;
-//stretch goals
+//made an added ittem function and created a basket
+//beginnging stretch goals
 
-function isfull();
+let basket = [];
+const maxItems = 5;
+
+function isFull(){
  if ( basket.length < maxItems){
     return false;
- } 
-    else if (basket.length >= maxItems){
-        return true;
  }
+    else {
+        return true;
+    }
+ }
+ console.log('basket')
+
+function addItem( item){
+    if (isFull()  === true){
+        console.log(' Your basket is full!')
+        return false;
+    } else {
+        basket.push(item)
+        return true;
+    }
+}
+//console.log testing
+
+console.log( 'adding oranges', addItem('oranges'));
+console.log( 'adding grapes', addItem('grapes'));
+console.log( 'adding cantalooupe', addItem('cantaloupe'));
+
+function listItems() {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
